caedfdbae8d59d08fc47e4b35a8fc020677aecf5
Dyshine Hill <eyedolarts@gmail.com>
2022-06-25 14:00:30 -0500

    completd Toggle the displaying of that paragraph with the button created in the

--- basics-assignment-3-start/src/app/app.component.ts
+++ basics-assignment-3-start/src/app/app.component.ts
@@ -1,9 +1,28 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styles: [
+    `
+      .five {
+        color: yellow;
+      }
+    `,
+  ],
 })
 export class AppComponent {
+  paragraph = true;
+
+  clicks = 0;
+  usersClicks = [0];
+  onchangeparagraph() {
+    this.paragraph = !this.paragraph;
+    console.log(this.usersClicks);
+    this.usersClicks.push((this.clicks = this.clicks + 1));
+  }
+  getColor() {
+    return this.usersClicks.length > 4 ? "blue" : "white";
+    console.log(this.usersClicks.length);
+  }
 }
